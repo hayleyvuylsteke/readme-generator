f5bf482703c9bf44eb517cb2aff7b61add635dd9
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-02-03 15:48:51 -0500

    readme file structure written

--- src/page-template.js
+++ src/page-template.js
@@ -12,7 +12,34 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${projectData.projectTitle}
+
+  ## Description
+  ${projectData.projectDescription}
+
+  ## Table of Contents
+  1. Installation
+  2. Usage
+  3. License
+  4. Contributing
+  5. Tests
+  6. Questions
+
+  ## Installation
+  ${projectData.projectInstallation}
+
+  ## Usage
+  ${projectData.projectUsage}
+
+  ## Licencing
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+  If you have questions about this project, please reach me by email at [${projectData.projectOwnerEmail}](mailto:${projectData.projectOwnerEmail}). You can find me on GitHub [here](https://github.com/${projectData.projectOwnerGitUserName}).
+
 
 `;
 }
